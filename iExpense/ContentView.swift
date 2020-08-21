2182cab42b55265d47fb5da617868f5771089ca8
Matthew Richardson <mat_richardson@mac.com>
2020-08-21 10:15:45 +1000

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Matthew Richardson on 21/8/20.
+//  Copyright © 2020 Matthew Richardson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
